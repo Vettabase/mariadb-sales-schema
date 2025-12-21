664270229073245e994e2fdd03bcc4eda70a9aa7
Federico Razzoli <federico.razzoli@vettabase.com>
2025-12-21 02:27:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# MariaDB Sales Schema
+
+This is a MariaDB schema for tests and examples.
+
+Have fun with it and please contribute.
+
+
+## Project Purpose and Guidelines
+
+This schema is designed for tests and examples.
+
+Some possible use cases are the following:
+
+- Students and new users can use it to understand database design good practices.
+- Tools and library developers can use it to test support for various
+  MariaDB features.
+- Developers and database professionals can use it to experiment with
+  various MariaDB features.
+- It can be used to test the impact on some indexing practices or some other
+  features on MariaDB performance.
+- Easily writing examples for training courses, blog posts, books, etc.
+
+In general, we should follow the good practices and use advanced features
+for a high-quality OLTP schema. But since part of our goals is testing
+performance and features, we'll also include more common practices, and
+even some poor design practices. We will document poor practices used.
+
+
+## Setup
+
+To create this schema, run `schemas.sql` through the mariadb CLI client.
+To avoid confusion, remember: MariaDB is the database server, whereas
+mariadb (lowercase) is the CLI client that is usually installed with MariaDB.
+
+Make sure to pass your MariaDB credentials to the client in one of these ways:
+
+- If you are `root` in MariaDB and you're logged into the local Linux OS as `root`,
+  you probably won't need to specify any credentials. By default, local `root`
+  connections are authenticated via
+  [UNIX_SOCKET](https://mariadb.com/docs/server/reference/plugins/authentication-plugins/authentication-plugin-unix-socket).
+- Otherwise, make sure that the credentials are set in your user's home directory's
+  `.my.ini` or in `/etc/mysql/my.ini`. See the
+  [documentation](https://mariadb.com/docs/server/server-management/install-and-upgrade-mariadb/configuring-mariadb/configuring-mariadb-with-option-files).
+- Alternartively, you can pass the credentials to mariadb using CLI options.
+  See the
+  [documentation](https://mariadb.com/docs/server/clients-and-utilities/mariadb-client/mariadb-command-line-client).
+
+To pass the `schema.sql` file to mariadb, run:
+
+```
+mariadb [credentials] < schema.sql
+```
+
+Some people are uncomfortable with mariadb's interface.
+
+Before switching to a GUI or a web UI, try [mycli](https://www.mycli.net/).
+It's still a CLI, but it provides user-friendly features like autocompletion, syntax
+highlighting, and saved queries. The usage is very similar.
+To pass the `schema.sql` file to mariadb, run:
+
+```
+mycli [credentials] < schema.sql
+```
+
+
+## Credits
+
+This schema was developed by [vettabase](https://vettabase.com).
+
+Current maintainer: [Federico Razzoli](federico.razzoli@vettabase.com).
+
+Contributions are welcome.
+
+
+## Copyright and Lucense
+
+License: GNU Affero GPL v3. See the `LICENSE` file.
+SPDX: [AGPL-3.0-only](http://spdx.org/licenses/AGPL-3.0-only.html).
+
+Copyright Vettabase Ltd 2025
+
---
+++ schema.sql
@@ -0,0 +1,179 @@
+--  SPDX-License-Identifier: AGPL-3.0-only
+--  Copyright (C) 2025 Vettabase info@vettabase.com
+
+
+--  MariaDB Sales Schema
+--  ====================
+--
+--  This is a MariaDB schema for tests and examples,
+--  maintained by Vettabase (vettabase.com).
+--  Have fun with it and please contribute.
+
+
+DROP SCHEMA sales_xmp;
+CREATE SCHEMA sales_xmp
+    DEFAULT CHARACTER SET = utf8mb4
+    DEFAULT COLLATE = utf8mb4_uca1400_ai_ci
+    COMMENT = 'Hypothetical online store. To be used for tests and examples.'
+;
+USE sales_xmp;
+
+
+CREATE TABLE customer (
+    uuid UUID DEFAULT UUID_v7(),
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_email (email)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'Any person or company that engaged with a sale process'
+;
+
+CREATE TABLE person (
+    uuid UUID DEFAULT UUID_v7(),
+    customer_uuid UUID NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_customer_uuid (customer_uuid),
+    FOREIGN KEY (customer_uuid) REFERENCES customer(uuid) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+    WITH SYSTEM VERSIONING
+    ENGINE=InnoDB
+    COMMENT 'A person who is also a customer'
+;
+
+CREATE TABLE company (
+    uuid UUID DEFAULT UUID_v7(),
+    customer_uuid UUID NOT NULL,
+    company_name VARCHAR(255) NOT NULL,
+    tax_id VARCHAR(50),
+    registration_number VARCHAR(50),
+    PRIMARY KEY (uuid),
+    UNIQUE unq_customer_uuid (customer_uuid),
+    FOREIGN KEY (customer_uuid) REFERENCES customer(uuid) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'A company who is also a customer'
+;
+
+CREATE TABLE contact_type (
+    uuid UUID DEFAULT UUID_v7(),
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_name (name)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'Supported contact types, like emails or phone numbers'
+;
+
+CREATE TABLE contact (
+    uuid UUID DEFAULT UUID_v7(),
+    contact_type_uuid UUID NOT NULL,
+    contact_value VARCHAR(255) NOT NULL,
+    PRIMARY KEY (uuid),
+    FOREIGN KEY (contact_type_uuid) REFERENCES contact_type(uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    INDEX idx_contact_type_uuid (contact_type_uuid)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'A contact other than the primary email, like a phone number or a messenger id'
+;
+
+CREATE TABLE person_contact (
+    uuid UUID DEFAULT UUID_v7(),
+    person_uuid UUID NOT NULL,
+    contact_uuid UUID NOT NULL,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_person_uuid_contact_uuid (person_uuid, contact_uuid),
+    FOREIGN KEY (person_uuid) REFERENCES person(uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    FOREIGN KEY (contact_uuid) REFERENCES contact(uuid) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'Link between a person and a contact'
+;
+
+CREATE TABLE company_contact (
+    uuid UUID DEFAULT UUID_v7(),
+    company_uuid UUID NOT NULL,
+    contact_uuid UUID NOT NULL,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_company_uuid_contact_uuid (company_uuid, contact_uuid),
+    FOREIGN KEY (company_uuid) REFERENCES company(uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    FOREIGN KEY (contact_uuid) REFERENCES contact(uuid) ON DELETE CASCADE ON UPDATE RESTRICT
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'Link between a company and a contact'
+;
+
+CREATE TABLE product (
+    uuid UUID DEFAULT UUID_v7(),
+    sku VARCHAR(50) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_sku (sku)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'A product that we sell'
+;
+
+CREATE TABLE sales_order_status (
+    uuid UUID DEFAULT UUID_v7(),
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    PRIMARY KEY (uuid),
+    UNIQUE unq_name (name)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'Status code for orders'
+;
+
+CREATE TABLE sales_order (
+    uuid UUID DEFAULT UUID_v7(),
+    customer_uuid UUID NOT NULL,
+    order_status_uuid UUID NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (uuid),
+    FOREIGN KEY (customer_uuid) REFERENCES customer (uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    FOREIGN KEY (order_status_uuid) REFERENCES sales_order_status (uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    INDEX idx_customer_uuid (customer_uuid),
+    INDEX idx_order_status_uuid (order_status_uuid),
+    INDEX idx_created_at (created_at)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'An order made by a customer'
+;
+
+CREATE TABLE order_line (
+    uuid UUID DEFAULT UUID_v7(),
+    order_uuid UUID NOT NULL,
+    product_uuid UUID NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    unit_price DECIMAL(10, 2) NOT NULL,
+    line_total DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (uuid),
+    UNIQUE unq_order_uuid_product_uuid (order_uuid, product_uuid),
+    FOREIGN KEY (order_uuid) REFERENCES sales_order (uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    FOREIGN KEY (product_uuid) REFERENCES product (uuid) ON DELETE CASCADE ON UPDATE RESTRICT,
+    INDEX idx_order_uuid (order_uuid)
+)
+    WITH SYSTEM VERSIONING
+    ENGINE InnoDB
+    COMMENT 'An entry in an order: product + quantity'
+;
